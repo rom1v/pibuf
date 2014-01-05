598b1218408f82056ba61a86cd3126ed6ee6b4ea
Romain Vimont (®om) <rom@rom1v.com>
2014-01-05 22:29:38 +0100

    Keep headers
    Read headers at the very top of the stream, and keep them in the output
    stream.

--- main.c
+++ main.c
@@ -1,17 +1,26 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 #include <errno.h>
 #include "cbuf.h"
+#include "naldetector.h"
 
 #define BUFSIZE 4096 //for reading stdin
+#define HEADERS_MAXSIZE 32 // must be lower than BUFSIZE
+#define PAYLOAD_START_PREFIX_NUMBER 3 // assume there are 2 NALs in headers
 
 int main(int argc, char *argv[]) {
   struct cbuf cbuf;
   char buf[BUFSIZE];
   long cbufsize;
   int read;
-  char *garbage = NULL;
+  char *garbage;
+  char *headers;
+  int headers_size;
+  struct naldetector detector;
+  int remaining_start_prefixes;
 
+  // parse commandline arguments
   if (argc == 1) {
     cbufsize = 10 * 1024 * 1024; // 10mb
   } else if (argc == 2) {
@@ -30,16 +39,54 @@ int main(int argc, char *argv[]) {
     fprintf(stderr, "Syntax: %s [cbufsize in kb]\n", argv[0]);
     exit(1);
   }
+
+  // init circular buffer
   if (cbuf_init(&cbuf, cbufsize)) {
     fprintf(stderr, "Out of memory");
     exit(2);
   }
-  while (!feof(stdin) && !ferror(stdin)) {
-    if ((read = fread(buf, 1, BUFSIZE, stdin))) {
-      cbuf_write(&cbuf, buf, read);
+
+  // read headers
+  // detect the i-th start prefix (0x000001), everything before is headers
+  naldetector_init(&detector);
+  headers_size = 0;
+  remaining_start_prefixes = PAYLOAD_START_PREFIX_NUMBER;
+  while (!feof(stdin) && !ferror(stdin) && remaining_start_prefixes > 0) {
+    if ((read = fread(&buf[headers_size], 1, HEADERS_MAXSIZE - headers_size, stdin))) {
+      while (read > 0 && remaining_start_prefixes > 0) {
+        read--;
+        headers_size++;
+        if (naldetector_eat(&detector, buf[headers_size - 1])) {
+          if (--remaining_start_prefixes == 0) {
+            headers_size -= 3;
+            if (!(headers = malloc(headers_size))) {
+              perror("Cannot allocate headers buffer");
+              exit(2);
+            }
+            memcpy(headers, buf, headers_size);
+            // push the remaining data, including 0x000001, into the circular buffer
+            cbuf_write(&cbuf, &buf[headers_size], read + 3);
+          }
+        }
+      }
     }
   }
-  cbuf_print_nals(&cbuf, stdout);
+
+  if (remaining_start_prefixes == 0) {
+    // the headers have been detected,
+    fprintf(stderr, "headers_size = %d\n", headers_size);
+
+    // consume
+    while (!feof(stdin) && !ferror(stdin)) {
+      if ((read = fread(buf, 1, BUFSIZE, stdin))) {
+        cbuf_write(&cbuf, buf, read);
+      }
+    }
+    // write headers
+    fwrite(headers, 1, headers_size, stdout);
+    // write cbuf data
+    cbuf_print_nals(&cbuf, stdout);
+  }
   cbuf_free(&cbuf);
   return 0;
 }
